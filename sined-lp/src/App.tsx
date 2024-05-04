66cd319ac9486ba4c4e4d96aba3c035f881ad620
Cavalcante <joseluismoreiracavalcante@gmail.com>
2024-05-04 19:19:26 -0300

    Adicionar Tailwind e botão de inscrição

--- sined-lp/src/App.tsx
+++ sined-lp/src/App.tsx
@@ -1,9 +1,18 @@
-import { useState } from 'react'
+import { useRef } from 'react'
 import './App.scss'
 import Form from './app/pages/Form'
+import { Button } from "flowbite-react";
+import './index.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const sectionRef = useRef<HTMLElement>(null);
+
+  // Função para realizar o scroll até a seção
+  const scrollToSection = () => {
+    if (sectionRef.current) {
+      sectionRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  };
 
   return (
     <div className='app'>
@@ -20,13 +29,20 @@ function App() {
           </div>
         </div>
 
-        <div className="title">
+        <div className="uppercase mt-16 flex flex-col gap-4 text-center text-4xl font-bold tracking-tight text-white-900 sm:text-6xl">
           <span>
             Simpósio Nacional
           </span>
           <span>
             de Educação
           </span>
+        </div>
+        <div className='w-full my-8 flex flex-row items-center justify-center'>
+          <Button size="lg" gradientDuoTone="greenToBlue" className='shadow-md' onClick={scrollToSection}>
+            <span className='text-xl'>
+              Realizar Inscrição   
+            </span>
+          </Button>
         </div>
       </div>
 
@@ -70,7 +86,7 @@ function App() {
           </div>
         </div>
       </section>
-      <section>
+      <section ref={sectionRef}>
           <Form />
       </section>
     </div>

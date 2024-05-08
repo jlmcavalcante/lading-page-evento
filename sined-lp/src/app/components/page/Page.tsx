36f6466d6b7c2e848f259b327953dc730ec4f5d4
Cavalcante <joseluismoreiracavalcante@gmail.com>
2024-05-08 03:43:27 -0300

    Adicionar título à seção de apoiadores

--- sined-lp/src/app/components/page/Page.tsx
+++ sined-lp/src/app/components/page/Page.tsx
@@ -153,9 +153,17 @@ export default function Page() {
       </section>
 
       <div className="min-h-0 h-64">
-        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+        <div className="content-container">
+          <div className="flex flex-col w-full pt-8">
+            <span className="text-2xl font-medium text-[#016D94] uppercase">
+              Realizadores e Apoiadores
+            </span>
+            <div className="bg-[#016D94] w-full h-1 rounded-r-lg"></div>
+          </div>
+        </div>
+        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 px-16">
           <Carousel slide={true} theme={customCarouselTheme}>
-            <div className="flex h-full items-center justify-center bg-white dark:bg-gray-700 dark:text-white">
+            <div className="flex h-full items-center justify-center bg-white dark:bg-gray-700 dark:text-white px-4">
               <img
                 src="/apoiador-1.jpeg"
                 alt="..."
@@ -175,20 +183,14 @@ export default function Page() {
                 className="flex-1 scale-50"
               />
               <img
-                src=""
-                alt=""
-                width={1/3}
-                className="flex-1 scale-50"
-              />
-            </div>
-
-            <div className="flex h-full items-center justify-center bg-white dark:bg-gray-700 dark:text-white">
-              <img
                 src="/apoiador-4.jpeg"
                 alt="..."
                 width={1/3}
                 className="flex-1 scale-50"
               />
+            </div>
+
+            <div className="flex h-full items-center justify-center bg-white dark:bg-gray-700 dark:text-white px-4">
               <img
                 src="/apoiador-5.jpeg"
                 alt="..."
@@ -204,6 +206,12 @@ export default function Page() {
               <img
                 src="/apoiador-7.jpeg"
                 alt="..."
+                width={1/3}
+                className="flex-1 scale-50"
+              />
+              <img
+                src=""
+                alt=""
                 width={1/3}
                 className="flex-1 scale-50"
               />

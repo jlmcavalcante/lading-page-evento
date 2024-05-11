4ee4305f97c6bda425e8a758e8d0bc5e7528b3ae
jlmcavalcante <joseluismoreiracavalcante@gmail.com>
2024-05-10 21:20:24 -0300

    Corrigir campos do formulário

--- sined-lp/src/app/components/form/Form.tsx
+++ sined-lp/src/app/components/form/Form.tsx
@@ -5,14 +5,12 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Modal } from "flowbite-react";
-import { 
-  FormTitle,
-} from "./FormStyles";
-
+import { FormTitle } from "./FormStyles";
 
 // Schema: representação de uma estrutura de dados (objeto gerado do formulário).
 const createUserFormSchema = z.object({
-  name: z.string()
+  name: z
+    .string()
     .min(1, "O nome é obrigatório")
     .transform((name) => {
       return name
@@ -24,23 +22,32 @@ const createUserFormSchema = z.object({
         })
         .join(" ");
     }),
-  cpf: z.string().min(11, 'Insira no mínimo 11 números').max(14, 'Insira no máximo 14 caracteres'),
-  email: z.string()
+  cpf: z
+    .string()
+    .min(11, "Insira no mínimo 11 números")
+    .max(14, "Insira no máximo 14 caracteres"),
+  email: z
+    .string()
     .min(1, "O e-mail é obrigatório") // Validação de campo obrigatório
     .email("Formato de e-mail inválido") // Validação de email.
     .toLowerCase(),
-  nascimento: z.coerce.string().min(10, 'Insira sua data de nascimento.').date(),
-  cidade: z.string().min(1, 'Selecione a sua cidade'),
-  estado: z.string().min(1, 'Selecione o seu estado'),
+  nascimento: z.coerce
+    .string()
+    .min(10, "Insira sua data de nascimento.")
+    .date(),
+  cidade: z.string().min(1, "Selecione a sua cidade"),
+  estado: z.string().min(1, "Selecione o seu estado"),
+  poder: z.string().min(1),
+  especificacao: z.string().min(1, "Insira uma especificação"),
 });
 
 function todosAtributosPreenchidos(obj: any): boolean {
   for (const key in obj) {
-      if (obj.hasOwnProperty(key)) {
-          if (obj[key] === null || obj[key] === undefined) {
-              return false;
-          }
+    if (obj.hasOwnProperty(key)) {
+      if (obj[key] === null || obj[key] === undefined) {
+        return false;
       }
+    }
   }
   return true;
 }
@@ -52,13 +59,17 @@ export default function Form() {
   const [isTermsAccepted, setIsTermsAccepted] = useState(false);
   const [openModal, setOpenModal] = useState(false);
 
-  function createUser(data: any) {
-  }
+  function createUser(data: any) {}
 
-  const {register, handleSubmit, formState: { errors, isValid }, watch} = useForm<CreateUserFormData>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    watch,
+  } = useForm<CreateUserFormData>({
     // Objeto de configuração para reconhecimento das regras de validação.
     resolver: zodResolver(createUserFormSchema),
-    mode: 'onChange',  // Validação em tempo real.
+    mode: "onChange", // Validação em tempo real.
   });
 
   const watchedValues = watch();
@@ -76,9 +87,7 @@ export default function Form() {
   return (
     <div className="content-container flex-col">
       <div className="py-8">
-        <FormTitle>
-          Inscrição
-        </FormTitle>
+        <FormTitle>Inscrição</FormTitle>
       </div>
 
       <form onSubmit={handleSubmit(createUser)} className="flex flex-col">
@@ -120,9 +129,7 @@ export default function Form() {
               />
               {errors.email && <span className="">{errors.email.message}</span>}
             </div>
-          </div>
 
-          <div className="flex flex-col gap-4 flex-1 px-8">
             <div>
               <div className="mb-2 block">
                 <Label htmlFor="nascimento" value="Data de Nascimento:" />
@@ -133,9 +140,11 @@ export default function Form() {
                 <span className="">{errors.nascimento.message}</span>
               )}
             </div>
+          </div>
 
+          <div className="flex flex-col gap-4 flex-1 px-8">
             {/* Container para input de cidade e estado */}
-            <div className="flex gap-8 flex-1">
+            <div className="flex gap-8">
               <div className="flex-1">
                 <div className="mb-2 block">
                   <Label htmlFor="estado" value="Estado:" />
@@ -164,18 +173,28 @@ export default function Form() {
                 )}
               </div>
             </div>
-            <div className="mb-2 block">
-                <Label htmlFor="Poder" value="Poder:" />
+
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="poder" value="Poder:" />
               </div>
-            <Select id="poder" {...register("poder")} defaultValue={""}>
-                  <option> </option>
-                  <option>Estudante</option>
-                  <option>Professor</option>
-                  <option>Coordenador</option>
-                  <option>Outro</option>
+              <Select id="poder" {...register("poder")} defaultValue={""}>
+                <option> </option>
+                <option>Estudante</option>
+                <option>Professor</option>
+                <option>Coordenador</option>
+                <option>Outro</option>
               </Select>
+            </div>
+            
+            <div>
               <Label htmlFor="Especificação" value="Especificação:" />
-              <TextInput type="text" placeholder="especifique" {...register("especificacao")} />
+              <TextInput
+                type="text"
+                placeholder="Especifique"
+                {...register("especificacao")}
+              />
+            </div>
           </div>
         </div>
 
@@ -191,39 +210,26 @@ export default function Form() {
         </div>
       </form>
 
-
       <Modal show={openModal} onClose={() => setOpenModal(false)}>
         <Modal.Header>Termos de Serviço</Modal.Header>
         <Modal.Body>
           <div className="space-y-6">
             <div className="text-base flex flex-col leading-relaxed text-gray-500 dark:text-gray-400">
-              <span>
-                {watchedValues.name} 
-              </span>
-              <span>
-                {watchedValues.email}
-              </span>
-              <span>
-                {watchedValues.cpf}
-              </span>
-              <span>
-                {watchedValues.nascimento}
-              </span>
-              <span>
-                {watchedValues.estado}
-              </span>
-              <span>
-                {watchedValues.cidade}
-              </span>
+              <span>{watchedValues.name}</span>
+              <span>{watchedValues.email}</span>
+              <span>{watchedValues.cpf}</span>
+              <span>{watchedValues.nascimento}</span>
+              <span>{watchedValues.estado}</span>
+              <span>{watchedValues.cidade}</span>
             </div>
             <div className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
               <div className="flex items-center gap-2">
-                <Checkbox 
-                  id="accept" 
+                <Checkbox
+                  id="accept"
                   checked={isTermsAccepted}
                   onChange={(e) => setIsTermsAccepted(e.target.checked)}
                 />
-                <Label htmlFor="accept" className="flex" >
+                <Label htmlFor="accept" className="flex">
                   Li e concordo com os&nbsp;
                   <a
                     href="#"
@@ -237,7 +243,12 @@ export default function Form() {
           </div>
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={() => setOpenModal(false)} disabled={!(isValid && isTermsAccepted)}>Confirmar</Button>
+          <Button
+            onClick={() => setOpenModal(false)}
+            disabled={!(isValid && isTermsAccepted)}
+          >
+            Confirmar
+          </Button>
           <Button color="gray" onClick={() => setOpenModal(false)}>
             Cancelar
           </Button>

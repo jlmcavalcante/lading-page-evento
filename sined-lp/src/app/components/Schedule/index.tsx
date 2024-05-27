32b59d123a97e4797b5e843d116d3f671b4e98f6
Gabriel Coelho <gabrielcoelho09gc@gmail.com>
2024-05-27 17:07:04 -0300

    Schedule Update

--- sined-lp/src/app/components/Schedule/index.tsx
+++ sined-lp/src/app/components/Schedule/index.tsx
@@ -27,6 +27,11 @@ const Schedule: React.FC = () => {
                     description: "",
                     subcontent: [
                         {
+                            title: "O Sistema Nacional de Educação e a Governança Colaborativa: principais desafios",
+                            description: "Luiz Fernando Abrucio – Professor FGV",
+                            location: "Teatro SESC",
+                        },
+                        {
                             title: "Apresentação cultural",
                             description: "Luar do São João",
                             location: "Teatro SESC",
@@ -49,16 +54,16 @@ const Schedule: React.FC = () => {
                     location: "Teatro SESC",
                     title: "Painel 1",
                     mainSpeaker: {
-                        name: "Lilian Martins",
-                        title: "TCE/PI",
+                        name: "Cons. Patrícia Sarmento",
+                        title: "TCE/MS",
                     },
                     speakers: [
-                        "Maurício Holanda – Secretário da Secretaria de Articulação Intersetorial e com os Sistemas de Ensino do Ministério da Educação (SASE/MEC)",
-                        "Idilvan Alencar – Deputado Federal",
-                        "Alessio Costa Lima – Presidente da Undime Nacional",
-                        "Mediação: Lilian Martins - TCE/PI",
+                        "Washington Bandeira – Secretário de estado da Educação do Piauí",
+                        "Vitor de Ângelo - Secretário de estado da Educação do Espírito Santo",
+                        "Jucineide Fernandes - Secretária Executiva do Ensino Médio e Profissional (SEDUC/CE)",
+                        "Mediação: Cons. Patrícia Sarmento - TCE/MS",
                     ],
-                    theme: "Sistema Nacional de Educação",
+                    theme: "Regime de Colaboração",
                     description: "",
                     subcontent: [],
                 },
@@ -68,16 +73,17 @@ const Schedule: React.FC = () => {
                     location: "Teatro SESC",
                     title: "Painel 2",
                     mainSpeaker: {
-                        name: "Cons. Patrícia Sarmento",
-                        title: "TCE/MS",
+                        name: "Flávia Gomes Cordeiro",
+                        title: "MP/PI",
                     },
                     speakers: [
-                        "Washington Bandeira – Secretário de estado da Educação do Piauí",
-                        "Vitor de Ângelo – Secretário de estado da Educação do Espírito Santo",
-                        "Eliana Nunes – Secretária de estado da Educação do Ceará",
-                        "Mediação: Cons. Patrícia Sarmento - TCE/MS",
+                        "João Augusto Bandeira de Mello – MPC/SE e Presidente da AMPCON",
+                        "Rodrigo Coelho do Carmo – Conselheiro do TCE/ES e Presidente do CTE-IRB",
+                        "Alípio Dias do Santos Neto – Auditor do TCU",
+                        "Fredy Henrique Miller – Auditor do TCM/SP",
+                        "Mediação: Flávia Gomes Cordeiro - MP/PI",
                     ],
-                    theme: "Regime de Colaboração",
+                    theme: "Experiências do controle externo",
                     description: "",
                     subcontent: [],
                 },
@@ -91,7 +97,7 @@ const Schedule: React.FC = () => {
                     },
                     speakers: [],
                     theme: "",
-                    description: "",
+                    description: "Atividade: Game",
                     subcontent: [],
                 },
                 {
@@ -100,17 +106,17 @@ const Schedule: React.FC = () => {
                     location: "Teatro SESC",
                     title: "Painel 3",
                     mainSpeaker: {
-                        name: "Cons. Suzana",
+                        name: "Cons. Suzana Maria Fontes Azevedo Freitas",
                         title: "TCE/SE",
                     },
                     speakers: [
-                        "Ivo Gomes – Prefeito de Sobral/CE",
-                        "Marcelo Beltrão Siqueira – Prefeito de Cururipe/AL",
+                        "Herbet Lima– Secretário municipal de Educação de Sobral/CE",
+                        "Juliana Rohsner Vianna Toniati – Secretária municipal de Educação de Vitória/ES",
                         "Danilo Alves de Carvalho – Prefeito de Itabaianinha/SE",
                         "José Raimundo de Sá Lopes – Prefeito de Oeiras/PI",
-                        "Mediação: Cons. Suzana - TCE/SE"
+                        "Mediação: Cons. Suzana Maria Fontes Azevedo Freitas - TCE/SE"
                     ],
-                    theme: "Experiências municipais, reordenamento de rede e resultados",
+                    theme: "Tema: Experiências municipais, reordenamento de rede e resultados (prefeitos)",
                     description: "",
                     subcontent: [],
                 },
@@ -124,7 +130,7 @@ const Schedule: React.FC = () => {
                     },
                     speakers: [],
                     theme: "",
-                    description: "",
+                    description: "Atividade: Game",
                     subcontent: [],
                 },
             ]
@@ -143,7 +149,7 @@ const Schedule: React.FC = () => {
                     },
                     speakers: [],
                     theme: "",
-                    description: "",
+                    description: "Camila",
                     subcontent: [],
                 },                
                 {
@@ -152,23 +158,20 @@ const Schedule: React.FC = () => {
                     location: "Teatro SESC",
                     title: "Painel 01",
                     mainSpeaker: {
-                        name:"Flávia Gomes Cordeiro",
-                        title: "MP/PI"
+                        name:"Lilian Martins",
+                        title: "TCE/PI"
                     },
-                    speakers: [
-                        "João Augusto Bandeira de Mello – MPC/SE e Presidente da AMPCON",
-                        "Rodrigo Coelho do Carmo – Conselheiro do TCE/ES e Presidente do CTE-IRB",
-                        "Alípio Dias do Santos Neto – Auditor do TCU",
-                        "Cíntia Regina Béo – Auditora do TCM/SP"
+                    speakers: [,
+                        "Mediação: Cons. Lilian Martins – TCE/PI"
 
                     ],
-                    theme: "Experiências do controle externo",
+                    theme: "PNE e Sistema Nacional de Educação",
                     description: "",
                     subcontent: [],
-                },                
+                },                      
                 {
                     date: "06/06/2024",
-                    time: "11:00",
+                    time: "14:00",
                     location: "",
                     title: "Blackbox 4",
                     mainSpeaker: {
@@ -177,21 +180,7 @@ const Schedule: React.FC = () => {
                     },
                     speakers: [],
                     theme: "",
-                    description: "",
-                    subcontent: [],
-                },                
-                {
-                    date: "06/06/2024",
-                    time: "14:00",
-                    location: "",
-                    title: "Blackbox 5",
-                    mainSpeaker: {
-                        name:"",
-                        title: ""
-                    },
-                    speakers: [],
-                    theme: "",
-                    description: "",
+                    description: "Atividade: Game",
                     subcontent: [],
                 },                
                 {
@@ -200,15 +189,15 @@ const Schedule: React.FC = () => {
                     location: "Teatro SESC",
                     title: "Painel 02",
                     mainSpeaker: {
-                        name:"Alessandra Gotti",
-                        title: "Presidente do Instituto Articule"
+                        name:"Rejane Dias",
+                        title: "Conselheira do TCE/PI"
                     },
                     speakers: [
                         "Olavo Nogueira Filho – Diretor do Todos Pela Educação",
-                        "Gregori Elacqua – Economista do BID",
                         "Mônica Rodrigues – Chefe da área de educação do UNICEF",
-                        "José Cordoveu de Arruda Neto – Diretor da Associação Bem Comum"
-
+                        "Rogers Mendes - Gestor da Área de Impacto Nacional do Centro Lemann",
+                        "Haroldo Correa Rocha – Coordenador Geral do Movimento Profissão Docente",
+                        "Mediação: Rejane Dias – Conselheira do TCE/PI",
                     ],
                     theme: "Experiências do Terceiro Setor",
                     description: "",
@@ -225,7 +214,7 @@ const Schedule: React.FC = () => {
                     },
                     speakers: [],
                     theme: "",
-                    description: "",
+                    description: "Premiação",
                     subcontent: [],
                 },
             ]
